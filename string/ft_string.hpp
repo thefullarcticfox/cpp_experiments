3e579709cf1835308ac4cf740f479dca5ad449d9
salec <salec@student.21-school.ru>
2020-12-09 18:21:58 +0300

    ft_stringto added+

--- string/ft_string.hpp
+++ string/ft_string.hpp
@@ -5,22 +5,33 @@
 #include <vector>
 
 /* ft_match			*/
-bool			ft_match(std::string const &s1, std::string const &s2);
+bool		ft_match(std::string const &s1, std::string const &s2);
 
 /* ft_splitstring	*/
 typedef std::vector<std::string>	t_strvect;
-t_strvect		ft_splitstring(std::string str, std::string const &delim);
-t_strvect		ft_splitstringbyany(std::string str, std::string const &delim);
+t_strvect	ft_splitstring(std::string str, std::string const &delim);
+t_strvect	ft_splitstringbyany(std::string str, std::string const &delim);
 
 /* ft_tostring		*/
-std::string		ft_tostring(int val);
-std::string		ft_tostring(long val);
-std::string		ft_tostring(long long val);
-std::string		ft_tostring(unsigned val);
-std::string		ft_tostring(unsigned long val);
-std::string		ft_tostring(unsigned long long val);
-std::string		ft_tostring(float val);
-std::string		ft_tostring(double val);
-std::string		ft_tostring(long double val);
+std::string			ft_tostring(int val);
+std::string			ft_tostring(long val);
+std::string			ft_tostring(long long val);
+std::string			ft_tostring(unsigned val);
+std::string			ft_tostring(unsigned long val);
+std::string			ft_tostring(unsigned long long val);
+std::string			ft_tostring(float val);
+std::string			ft_tostring(double val);
+std::string			ft_tostring(long double val);
+
+/* ft_stringto		*/
+int					ft_stoi(std::string str);
+long				ft_stol(std::string str);
+long long			ft_stoll(std::string str);
+unsigned int		ft_stou(std::string str);
+unsigned long		ft_stoul(std::string str);
+unsigned long long	ft_stoull(std::string str);
+float				ft_stof(std::string str);
+double				ft_stod(std::string str);
+long double			ft_stold(std::string str);
 
 #endif
